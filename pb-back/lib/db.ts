0f14cf3ceef1aab9224ae93a9c222ed34a4a7a10
rider <ridebus82@nate.com>
2026-06-17 14:28:31 +0900

    sdfasdfasdf

--- pb-back/lib/db.ts
+++ pb-back/lib/db.ts
@@ -3,6 +3,7 @@ import mysql from "mysql2"
 
 export const sql_con = mysql.createPool({
     host: process.env.HOST || '127.0.0.1',
+    port: Number(process.env.DBPORT) || 3306,
     user: 'root',
     password: process.env.DBPWD,
     database: process.env.SHEMA,
